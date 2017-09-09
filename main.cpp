d2643171db82341bec5d6b21f7db1e1c1b349103
Peter Arato <it.arato@gmail.com>
2017-09-09 19:41:06 -0400

    AST builder still has a flaw where a rule match cannot make sure that it is a global match. We need to store which rule matched and if any retrial is necessary we just start from the next one.

--- main.cpp
+++ main.cpp
@@ -5,20 +5,37 @@
 #include <algorithm>
 #include <iterator>
 #include <map>
+#include <cstdio>
+
+#define RULE_T_NAME "T_NAME"
+#define RULE_T_NUMBER "T_NUMBER"
+#define RULE_T_ADD "T_ADD"
+#define RULE_T_SUB "T_SUB"
+#define RULE_T_MUL "T_MUL"
+#define RULE_T_DIV "T_DIV"
 
 using namespace std;
+
+class Token;
+
+typedef vector<Token>::iterator vec_iter_t;
 
 class GrammarRule {
 public:
     vector<string> parts;
+    bool is_one_or_more;
+
     void add_part(string part) { parts.push_back(part); };
 
     friend ostream & operator<<(ostream & os, const GrammarRule & self) {
+        if (self.is_one_or_more) os << "+";
         os << "{ ";
         copy(self.parts.begin(), self.parts.end(), ostream_iterator<string>(os, " "));
         os << "}";
         return os;
     };
+
+    void set_one_or_more() { is_one_or_more = true; }
 };
 
 class GrammarLine {
@@ -67,7 +84,11 @@ public:
                 grammar_line->add_rule(*grammar_rule);
                 grammar_rule = new GrammarRule();
             } else {
-                grammar_rule->add_part(word);
+                if (word == "+") {
+                    grammar_rule->set_one_or_more();
+                } else {
+                    grammar_rule->add_part(word);
+                }
             }
         }
         grammar_line->add_rule(*grammar_rule);
@@ -96,9 +117,210 @@ public:
     };
 };
 
+enum TokenType {
+    NAME,
+    NUMBER,
+    OP_ADD,
+    OP_SUB,
+    OP_MUL,
+    OP_DIV,
+    SEMICOLON,
+};
+
+ostream & operator<<(ostream & os, const TokenType & self) {
+    switch (self) {
+        case NAME:
+            os << "NAME";
+            break;
+        case NUMBER:
+            os << "NUMBER";
+            break;
+        case OP_ADD:
+            os << "OP_ADD";
+            break;
+        case OP_SUB:
+            os << "OP_SUB";
+            break;
+        case OP_MUL:
+            os << "OP_MUL";
+            break;
+        case OP_DIV:
+            os << "OP_DIV";
+            break;
+        case SEMICOLON:
+            os << "SEMICOLON";
+            break;
+    }
+    return os;
+}
+
+class Token {
+public:
+    TokenType type;
+    string value;
+
+    Token(TokenType type, string value) : type(type), value(value) {}; 
+
+    friend ostream & operator<<(ostream & os, const Token & self) {
+        os << self.type << "(" << self.value << ")";
+        return os;
+    }
+};
+
+class Tokenizer {
+public:
+    vector<Token> tokens;
+
+    Tokenizer() {
+        ifstream source_if("sample.p");
+      
+        for (;;) {
+            for (char top_ch = source_if.peek(); is_whitespace(source_if.peek()); source_if.get()) {}
+
+            if (source_if.peek() == EOF) break;
+
+            tokens.push_back(get_token(&source_if));
+        }
+    };
+
+    friend ostream & operator<<(ostream & os, const Tokenizer & self) {
+        os << "T[ ";
+        for (const auto token : self.tokens) {
+            os << token << " ";
+        }
+        os << "]";
+        return os;
+    };
+
+private:
+    Token get_token(ifstream *source_if) {
+        char ch = source_if->peek();
+        if (is_numeric(ch)) {
+            return read_number_token(source_if);
+        } else if (is_letter(ch)) {
+            return read_name_token(source_if);
+        } else if (is_op(ch)) {
+            source_if->get();
+            return read_op_token(ch);
+        } else if (is_semicolon(ch)) {
+            source_if->get();
+            return Token(TokenType::SEMICOLON, string{ch});
+        } else {
+            cout << "Unexpected char: " << (int) ch << endl;
+            exit(EXIT_FAILURE);
+        }
+    };
+
+    Token read_number_token(ifstream *source_if) {
+        string value;
+        while (is_numeric(source_if->peek())) {
+            value.push_back(source_if->get());
+        }
+        return Token(TokenType::NUMBER, value);
+    };
+
+    Token read_name_token(ifstream *source_if) {
+        string value;
+        while (is_letter(source_if->peek())) {
+            value.push_back(source_if->get());
+        }
+        return Token(TokenType::NAME, value);
+    };
+
+    Token read_op_token(char ch) {
+        switch (ch) {
+            case '+':
+                return Token(TokenType::OP_ADD, string{ch});
+            case '-':
+                return Token(TokenType::OP_SUB, string{ch});
+            case '*':
+                return Token(TokenType::OP_MUL, string{ch});
+            case '/':
+                return Token(TokenType::OP_DIV, string{ch});
+            default:
+                cout << "Unknown OP: " << ch << endl;
+                exit(EXIT_FAILURE);
+        }
+    };
+
+    bool is_numeric(char ch) { return ch >= '0' && ch <= '9'; };
+    bool is_letter(char ch) { return ch >= 'a' && ch <= 'z'; };
+    bool is_op(char ch) { return ch == '+' || ch == '-' || ch == '*' || ch == '/'; };
+    bool is_whitespace(char ch) { return !(is_numeric(ch) || is_letter(ch) || is_op(ch) || is_semicolon(ch) || ch == EOF); };
+    bool is_semicolon(char ch) { return ch == ';'; }
+};
+
+class AstBuilder {
+public:
+    Grammar *grammar;
+    Tokenizer *tokenizer;
+
+    AstBuilder(Grammar *grammar, Tokenizer *tokenizer) : grammar(grammar), tokenizer(tokenizer) {};
+
+    bool build() {
+        vec_iter_t it = tokenizer->tokens.begin();
+        return try_grammar_line(&(grammar->lines["PROG"]), &it);
+    };
+
+private:
+    bool try_grammar_line(GrammarLine *gr_line, vec_iter_t *token_it) {
+        vec_iter_t *orig_token_it = token_it;
+
+        for (auto rule : gr_line->rules) {
+            if (try_grammar_rule(&rule, orig_token_it)) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    bool try_grammar_rule(GrammarRule *rule, vec_iter_t *token_it) {
+        vec_iter_t *orig_token_it = token_it;
+
+        for (const auto rule_part : rule->parts) {
+            if (is_token(rule_part)) {
+                if (is_token_match(rule_part, **token_it)) {
+                    (*token_it)++;
+                } else {
+                    token_it = orig_token_it;
+                    return false;
+                }
+            } else {
+                if (!try_grammar_line(&grammar->lines[rule_part], token_it)) {
+                    token_it = orig_token_it;
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    bool is_token(string rule_part) {
+        return rule_part.at(0) == 'T' && rule_part.at(1) == '_';
+    }
+
+    bool is_token_match(string rule_part, Token token) {
+        return 
+            (rule_part == RULE_T_NAME && token.type == TokenType::NAME) ||
+            (rule_part == RULE_T_NUMBER && token.type == TokenType::NUMBER) ||
+            (rule_part == RULE_T_ADD && token.type == TokenType::OP_ADD) ||
+            (rule_part == RULE_T_SUB && token.type == TokenType::OP_SUB) ||
+            (rule_part == RULE_T_MUL && token.type == TokenType::OP_MUL) ||
+            (rule_part == RULE_T_DIV && token.type == TokenType::OP_DIV);
+    }
+};
+
 int main() {
     Grammar g;
     cout << g << endl;
+
+    Tokenizer t;
+    cout << t << endl;
+
+    AstBuilder ab(&g, &t);
+    ab.build();
 
     exit(EXIT_SUCCESS);
 }
