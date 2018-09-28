4021757d657d62ccfbe58b2fa4ab6996839fe106
Peter Arato <it.arato@gmail.com>
2018-09-28 13:25:15 -0700

    Forward log args

--- logger.cpp
+++ logger.cpp
@@ -7,18 +7,19 @@
 using namespace std;
 
 void Logger::log_all(string &&kind, unsigned int color, string &&s) {
-  log(kind, color, s);
+  log(kind, color, forward<string>(s));
 }
 
-void Logger::info(string s) { log("INFO", 94, s); }
+void Logger::info(string &&s) { log("INFO", 94, forward<string>(s)); }
 
-void Logger::error(string s) { log("ERROR", 91, s); }
+void Logger::error(string &&s) { log("ERROR", 91, forward<string>(s)); }
 
-void StdLogger::log(string kind, unsigned int color, string s) {
+void StdLogger::log(string kind, unsigned int color, string &&s) {
   auto time_res = time(nullptr);
-  char mbstr[100];
-  if (strftime(mbstr, sizeof(mbstr), "%H:%M:%S", localtime(&time_res))) {
-    cout << "\x1B[1m" << mbstr << " -- ";
+  char date_str_buffer[100];
+  if (strftime(date_str_buffer, sizeof(date_str_buffer), "%H:%M:%S",
+               localtime(&time_res))) {
+    cout << "\x1B[1m" << date_str_buffer << " -- ";
   }
   cout << "\x1B[" << color << "m" << kind << "\x1B[0m -- \x1B[0m";
   cout << s << endl;
--- logger.h
+++ logger.h
@@ -7,29 +7,29 @@
 using namespace std;
 
 class Logger {
- public:
-  virtual void log(string, unsigned int, string) = 0;
+public:
+  virtual void log(string, unsigned int, string &&) = 0;
 
-  void info(string);
+  void info(string &&);
 
   template <typename T1, typename T2, typename... Args>
   inline void info(T1 &&lhs, T2 rhs, Args... args) {
     log_all(forward<string>("INFO"), 94, lhs, rhs, args...);
   }
 
-  void error(string);
+  void error(string &&);
 
   template <typename T1, typename T2, typename... Args>
   inline void error(T1 &&lhs, T2 rhs, Args... args) {
     log_all(forward<string>("ERROR"), 91, lhs, rhs, args...);
   }
 
- private:
+private:
   void log_all(string &&, unsigned int, string &&);
 
   template <typename T1, typename T2, typename... Args>
   inline void log_all(string &&kind, unsigned int color, T1 lhs, T2 rhs,
-                      Args&&... args) {
+                      Args &&... args) {
     ostringstream buf{};
     buf << lhs << " " << rhs;
     log_all(forward<string>(kind), color, buf.str(), args...);
@@ -37,6 +37,6 @@ class Logger {
 };
 
 class StdLogger : public Logger {
- public:
-  void log(string, unsigned int, string) final;
+public:
+  void log(string, unsigned int, string &&) final;
 };
